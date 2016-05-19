87964c74ef7e5c1acc3e8e8cf94c2862ea7d24e9
XabirHK <xabirkhan@gmail.com>
2016-05-20 02:35:50 +0600

    Introduction to Layout
    Here the use of layout and section and yield

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,8 +3,19 @@
     <head>
         <meta charset='UTF-8'>
         <title>Learning Laravel Layout</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+
+        @yield('header')
     </head>
+    
     <body>
-        
+        @yield('content')
+
+
+
+
+        @yield('footer')
     </body>
 </html>
