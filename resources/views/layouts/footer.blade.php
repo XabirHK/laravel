3b1e5b1d0f6d6629568d7b346b813ed1e440cced
XabirHK <xabirkhan@gmail.com>
2016-05-22 02:03:03 +0600

    Creating One Page Template
    Still Unstable Need to see the next working day

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,17 @@
+ <!-- jQuery -->
+    <script src="{{ URL::asset('asset/js/jquery.js')}}"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{ URL::asset('asset/js/bootstrap.min.js')}}"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="{{ URL::asset('asset/js/classie.js')}}"></script>
+    <script src="{{ URL::asset('asset/js/cbpAnimatedHeader.js')}}"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="{{ URL::asset('asset/js/jqBootstrapValidation.js')}}"></script>
+    <script src="{{ URL::asset('asset/js/contact_me.js')}}"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="{{ URL::asset('asset/js/agency.js')}}"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('layouts.master')
 
 @section('content')
     <h1>The welcome page goes here</h1>
